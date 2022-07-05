1170d53739b5ae3a2a5a22889d15ea3ef7add4b9
kamiltrela <trelaka@sheridancollege.ca>
2022-07-05 19:14:00 -0400

    Add project files.

---
+++ PigeonAndOstrich/Pigeon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigeonAndOstrich
+{
+    class Pigeon : Bird
+    {
+        public override Egg[] LayEggs(int numberOfEggs)
+        {
+            
+            Egg[] eggs = new Egg[numberOfEggs];
+            for (int i = 0; i < numberOfEggs; i++)
+            {
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            }
+            return eggs;            
+        }
+    }
+}
